494d318c6d78501b08d0430e5cca25b1c5e7f7a6
bladeshape <tekigod@mail.ru>
2022-11-27 04:22:13 +0300

    asdasd2

---
+++ task1.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class task1 {
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        Scanner iScanner = new Scanner(System.in);
+        System.out.print("\n\nВведите число: ");
+        int a = iScanner.nextInt();
+        System.out.print("Введите степень числа: ");
+        int b = iScanner.nextInt();
+
+        if (b == 0) {
+            System.out.println("Число " + a + " в нулевой степени = 1");
+        } else if (b == 1) {
+            System.out.println("Число " + a + " ^ " + b + " = " + a + " .");
+        } else {
+            double c = a;
+            for (int i = 1; i < Math.abs(b); i++) {
+                c = c * a;
+            }
+            if (0 < b) {
+                System.out.println("Число " + a + " ^ " + b + " = " + c + " .");
+            }
+            if (b < 0) {
+                System.out.println("Число " + a + " ^ " + b + " = " + 1 / c + " .");
+            }
+        }
+
+    }
+}
